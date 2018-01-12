a9b963bcb0eddad4ce2669da20b2112e5f3b3468
uael <lucas.abel@yandex.com>
2018-01-12 10:41:27 +0100

    removed todolist

--- include/msh.h
+++ include/msh.h
@@ -13,41 +13,6 @@
 #ifndef MSH_H
 # define MSH_H
 
-/*
-** TODO
-** [X] - builtin cd
-** [X] - builtin env
-** [X] - builtin setenv/export
-** [X] - builtin unsetenv
-** [X] - builtin echo
-** [X] - builtin exit
-** [X] - basic command
-** [X] - semicolon separator
-** [X] - shell script
-** [X] - pipe
-** [X] - redirection
-** [X] - redirection fd duplication
-** [X] - exit status
-** [/] - librl (readline library)
-** [/] - history (librl)
-** [/] - autocomplete (librl)
-** [/] - line edition (librl)
-** [ ] - file stream adaptation to librl
-** [ ] - lexing adaptation to librl
-** [ ] - quote expansion adaptation to librl
-** [ ] - heredoc adaptation to librl
-** [ ] - fixes empty heredoc
-** [ ] - proper syntax error
-** [ ] - pipe fixes in some cases ? (see shell tester)
-** [ ] - logical operators (BONUS)
-** [ ] - async operator (BONUS)
-** [ ] - variable expansion (BONUS)
-** [ ] - subshell expansion (BONUS)
-** [ ] - parenthesis (BONUS)
-** [ ] - #! binary interpretation (BONUS)
-** [ ] - &> redirection operator (BONUS)
-*/
-
 # include "msh/env.h"
 # include "msh/err.h"
 # include "msh/job.h"
