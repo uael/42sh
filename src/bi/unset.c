ad16c0b9bf76d3a914e7ab2add22dcceb0667242
cmalfroy <cmalfroy@student.42.fr>
2018-01-22 11:35:50 +0100

    unset

--- src/bi/unset.c
+++ src/bi/unset.c
@@ -11,3 +11,27 @@
 /* ************************************************************************** */
 
 #include "ush.h"
+
+inline int	sh_biunset(int ac, char **av, char **env)
+{
+	int i;
+
+	(void)env;
+	i = 0;
+	if (ac == 1)
+		ft_retf(NOP, "unset: not enough arguments");
+	while (++i < ac)
+	{
+		if (av[i][0] == '-')
+		{
+			if (av[i][1] != 'v' || av[i][1] != 'f')
+				ft_retf(NOP, "bad option: -%c", av[i][1]);
+			else if (!av[i + 1] || (ft_strcmp(av[i], "-") == 0 && !av[i + 1]))
+				ft_retf(NOP, "unset: not enough arguments");
+		}
+		else if (sh_varget(av[i]))
+			sh_varset(av[1], NULL);
+	}
+	sh_biunsetenv(ac, av, env);
+	return (YEP);
+}
