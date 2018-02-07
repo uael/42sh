a49f88a52e32e24a377b051ecc35ae1374445b4d
alucas- <alucas-@student.42.fr>
2018-02-07 20:25:21 +0100

    backquote tests

--- test/backquote.sh
+++ test/backquote.sh
@@ -1,4 +1,6 @@
 echo `echo "a       b           c          def	ghi"`
 echo `ls`
-echo `ls -lar`
+echo `ls -lar` | grep -o bg.c
 echo `ls -lartR` | grep -o bg.c
+echo oops`ls -l` | grep -o oopstotal
+echo $HOME`ls -l` | grep -o ${HOME}total
