aef32207d3fc9520a18ac427f5ba534e0fc987d5
uael <lucas.abel@yandex.com>
2018-02-12 10:04:26 +0100

    fixed tests related to syntax/parse error reporting

--- test/error_11.ex
+++ test/error_11.ex
@@ -1,7 +1,3 @@
 ush: Expected `<filename>' got `<newline>'
 1>&1 cat 2&>- <<EOL > /dev/null; 1<&- >lol cat 2>&1 <<EOF >
-oops
-EOL
-oops 2
-EOF
-                                                            ^
+                                                           ^
