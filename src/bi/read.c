0a48255e9efcc66042af56c52482a0a8a0a887e6
cmalfroy <cmalfroy@student.42.fr>
2018-02-12 19:40:06 +0100

    get right values of options, termios time and delimiter not working yet need test for fd

--- src/bi/read.c
+++ src/bi/read.c
@@ -19,23 +19,31 @@ enum				e_readopt
 	READ_D = 1 << 3,
 };
 
+typedef struct		s_readopt
+{
+	ssize_t 		timeout;
+	char 			delim;
+	ssize_t 		read_fd;
+}					t_readopt;
+
+static t_readopt	g_opt = {0, '\n', 0};
+
 static inline int	parsearg(int ac, char *av[], uint8_t *flags)
 {
-	int		i;
 	char	*a;
+	int		i;
 
 	i = 0;
 	while (++i < ac)
 		if (*av[i] == '-' && (a = av[i]))
-			while (*++a)
-				if (*a == 't')
-					*flags |= READ_T;
-				else if (*a == 'u')
-					*flags |= READ_U;
-				else if (*a == 'd')
-					*flags |= READ_D;
-				else
-					return (ft_retf(WUT, "read: %s: invalid option\n", a));
+			if (*++a  == 't' && (*flags |= READ_T))
+				av[++i] ? g_opt.timeout = ft_atoi(av[i]) : 0;
+			else if (*a == 'u' && (*flags |= READ_U))
+				av[++i] ? g_opt.read_fd = ft_atoi(av[i]) : 0;
+			else if (*a == 'd' && (*flags |= READ_D))
+				av[++i] && ft_strlen(av[i]) == 1 ? g_opt.delim = av[i][0] : 0;
+			else
+				return (ft_retf(WUT, "read: %s: invalid option\n", a));
 		else
 			return (i);
 	return (YEP);
@@ -65,25 +73,27 @@ inline static void	readarg(int ac, char **av, char *ln, int i)
 
 inline int			sh_biread(int ac, char **av, char **env)
 {
-	ssize_t rd;
-	t_ifs	ifs;
-	char	*ln;
-	int		i;
-	uint8_t	flags;
+	struct termios	term;
+	uint8_t			flags;
+	ssize_t 		rd;
+	t_ifs			ifs;
+	char			*ln;
+	int				i;
 
 	(void)env;
-	ft_ifsctor(&ifs, STDIN_FILENO);
+	ft_ifsctor(&ifs, (int)g_opt.read_fd);
 	flags = 0;
 	if ((i = parsearg(ac, av, &flags)) < 0)
 		return (EXIT_FAILURE);
-	if ((flags & READ_T))
-		;
+	term.c_cc[VTIME] = (cc_t)(g_opt.timeout * 10);
+	term.c_cc[VMIN] = (cc_t)(g_opt.timeout ? 0 : 1);
+	term.c_cc[VEOL] = (cc_t)g_opt.delim;
 	if ((rd = ft_ifschr(&ifs, 0, '\n', &ln)) < 0)
 		return (EXIT_FAILURE);
 	if (rd == 0)
 		return (EXIT_SUCCESS);
 	rd > 1 ? ln[rd - 1] = '\0' : 0;
-	ac == 1 ? sh_varset("REPLY", ln) : readarg(ac, av, ln, i);
+	ac == i ? sh_varset("REPLY", ln) : readarg(ac, av, ln, i);
 	ft_ifsdtor(&ifs);
 	return (YEP);
 }
