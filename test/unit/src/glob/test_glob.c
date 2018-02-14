4da35abb546838f4e255405f7c1214b934113357
JeanMax <mcanal@student.42.fr>
2018-02-14 21:11:06 +0100

    Improve test_glob II

--- test/unit/src/glob/test_glob.c
+++ test/unit/src/glob/test_glob.c
@@ -6,7 +6,7 @@
 /*   By: mcanal <zboub@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/29 15:38:59 by mcanal            #+#    #+#             */
-/*   Updated: 2018/02/14 15:28:21 by mc               ###   ########.fr       */
+/*   Updated: 2018/02/14 20:05:25 by mc               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,51 @@
 #include "ft_glob.h"
 #include <glob.h>
 
-static glob_t ctrl_glob_struct;
-
 struct s_test_glob {
 	char *pattern;
 	int flags;
 	t_glob pglob;
 };
+
+static char *glob_ret_strings[] = {
+	"GLOB_SUCCESS",
+	"GLOB_NOSPACE",
+	"GLOB_ABORTED",
+	"GLOB_NOMATCH",
+	"GLOB_NOSYS",
+};
+
+static char *glob_flags_strings[] = {
+	"GLOB_ERR",
+	"GLOB_MARK",
+	"GLOB_NOSORT",
+	"GLOB_DOOFFS",
+	"GLOB_NOCHECK",
+	"GLOB_APPEND",
+	"GLOB_NOESCAPE",
+	"GLOB_PERIOD",
+	"GLOB_MAGCHAR",
+	"GLOB_ALTDIRFUNC",
+	"GLOB_BRACE",
+	"GLOB_NOMAGIC",
+	"GLOB_TILDE",
+	"GLOB_ONLYDIR",
+	"GLOB_TILDE_CHECK",
+};
+
+#define BIG (1 << 16) //yolo
+#define SMALL (1 << 8) //yolo
+
+static char print_buffer[BIG] = {0};
+static char argv_input_buffer[BIG] = {0};
+static char argv_test_buffer[BIG] = {0};
+static char argv_ctrl_buffer[BIG] = {0};
+static char flags_buffer[SMALL] = {0};
+static char flags_input_buffer[SMALL] = {0};
+static char flags_test_buffer[SMALL] = {0};
+static char flags_ctrl_buffer[SMALL] = {0};
+
+static glob_t ctrl_glob_struct;
 
 static struct s_test_glob test_glob_arr[] = {
 	{"*ab*cd*", 0, {0, NULL, 0, 0}},
@@ -55,21 +93,166 @@ static struct s_test_glob test_glob_arr[] = {
 };
 
 
-static void glob_testux(char const *pat, int flags, t_glob *pglob)
-{
-	int expected = glob(pat, flags, NULL, &ctrl_glob_struct);
-	int ret = globctor(pat, flags, pglob);
 
-	if (ret != expected) {
-		MU_ASSERT(
-			ret == expected,
-			"glob return mismatch:\n    [pat='%s', flags='%d', test=%d, ctrl:%d, pglob={gl_pathc:%zu, gl_pathv:%zu, gl_offs:%zu, gl_flags:%d}]",
-			pat, flags, ret, expected,
-			pglob->gl_pathc, pglob->gl_pathv, pglob->gl_offs, pglob->gl_flags
-		);
+static void get_flags_string(int flags, char *buf)
+{
+	char *s;
+	size_t len;
+
+	if (!flags) {
+		strcpy(buf, "0");
+		return;
 	}
 
-	//TODO: compare pglob->{ac,__av__,offs,flags}
+	s = buf;
+	for (int i = 0; i <= 14; i++) {
+		if ((flags & (1 << i))) {
+			len = strlen(glob_flags_strings[i]);
+			memcpy(s, glob_flags_strings[i], len);
+			memcpy(s + len, " | ", 3);
+			s += len + 3;
+		}
+	}
+
+	*(s - 3) = 0;
+}
+
+static void get_argv_string(char **av, char *buf)
+{
+	char *s;
+	size_t len;
+
+	if (!av || !*av) {
+		*buf = 0;
+		return;
+	}
+
+	s = buf;
+	for (; *av; av++) {
+		len = strlen(*av);
+		memcpy(s, *av, len);
+		memcpy(s + len, ", ", 2);
+		s += len + 2;
+	}
+
+	*(s - 2) = 0;
+}
+
+static void get_debug_string(char const *pat, int flags, int ctrl, int test, \
+								  t_glob *glob_input, t_glob *glob_test, glob_t *glob_ctrl)
+{
+	get_flags_string(flags, flags_buffer);
+	get_flags_string(glob_input->gl_flags, flags_input_buffer);
+	get_flags_string(glob_test->gl_flags, flags_test_buffer);
+	get_flags_string(glob_ctrl->gl_flags, flags_ctrl_buffer);
+	get_argv_string(glob_input->gl_pathv, argv_input_buffer);
+	get_argv_string(glob_test->gl_pathv, argv_test_buffer);
+	get_argv_string(glob_ctrl->gl_pathv, argv_ctrl_buffer);
+
+	sprintf(
+		print_buffer,
+		CLR_GREEN "INPUT:" CLR_RESET "\n"
+		"    pattern=%s,\n"
+		"    flags=%s,\n"
+		"    pglob={\n"
+		"        gl_pathc:%zu,\n"
+		"        gl_pathv:[%s],\n"
+		"        gl_offs:%zu,\n"
+		"        gl_flags=%s,\n"
+		"    }\n"
+
+		CLR_YELLOW "CTRL:" CLR_RESET "\n"
+		"    return=%s,\n"
+		"    pglob={\n"
+		"        gl_pathc:%zu,\n"
+		"        gl_pathv:[%s],\n"
+		"        gl_offs:%zu,\n"
+		"        gl_flags=%s,\n"
+		"    }\n"
+
+		CLR_RED "TEST:" CLR_RESET "\n"
+		"    return=%s,\n"
+		"    pglob={\n"
+		"        gl_pathc:%zu,\n"
+		"        gl_pathv:[%s],\n"
+		"        gl_offs:%zu,\n"
+		"        gl_flags=%s,\n"
+		"    }\n",
+
+		pat,
+		flags_buffer,
+		glob_input->gl_pathc,
+		argv_input_buffer,
+		glob_input->gl_offs,
+		flags_input_buffer,
+
+		glob_ret_strings[ctrl],
+		glob_ctrl->gl_pathc,
+		argv_ctrl_buffer,
+		glob_ctrl->gl_offs,
+		flags_ctrl_buffer,
+
+		glob_ret_strings[test],
+		glob_test->gl_pathc,
+		argv_test_buffer,
+		glob_test->gl_offs,
+		flags_test_buffer
+	);
+}
+
+static void glob_testux(char const *pat, int flags, t_glob *pglob)
+{
+	t_glob input;
+	int ret, expected;
+
+	memcpy(&input, pglob, sizeof(t_glob));
+
+	expected = glob(pat, flags, NULL, &ctrl_glob_struct);
+	ret = globctor(pat, flags, pglob);
+
+	get_debug_string(pat, flags, expected, ret, &input, pglob, &ctrl_glob_struct);
+
+	MU_ASSERT(
+		ret == expected,
+		"glob return mismatch:\n" "%s",
+		print_buffer
+	);
+	if (ret != expected)
+		return;
+
+	MU_ASSERT(
+		pglob->gl_offs == ctrl_glob_struct.gl_offs,
+		"t_glob->gl_offs mismatch:\n" "%s",
+		print_buffer
+	);
+	if (pglob->gl_offs != ctrl_glob_struct.gl_offs)
+		return;
+
+	MU_ASSERT(
+		pglob->gl_flags == ctrl_glob_struct.gl_flags,
+		"t_glob->gl_flags mismatch:\n" "%s",
+		print_buffer
+	);
+	if (pglob->gl_flags != ctrl_glob_struct.gl_flags)
+		return;
+
+	MU_ASSERT(
+		pglob->gl_pathc == ctrl_glob_struct.gl_pathc,
+		"t_glob->gl_pathc mismatch:\n" "%s",
+		print_buffer
+	);
+	if (pglob->gl_pathc != ctrl_glob_struct.gl_pathc)
+		return;
+
+	for (int i = 0; pglob->gl_pathv && pglob->gl_pathv[i]; i++) {
+		MU_ASSERT(
+			!strcmp(pglob->gl_pathv[i], ctrl_glob_struct.gl_pathv[i]),
+			"t_glob->gl_pathv mismatch:\n" "%s",
+			print_buffer
+		);
+		if (strcmp(pglob->gl_pathv[i], ctrl_glob_struct.gl_pathv[i]))
+			return;
+	}
 }
 
 void test_glob()
