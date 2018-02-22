1b5753ba85ac216ccfa13cc96507ff54aacc17dd
uael <lucas.abel@yandex.com>
2018-02-22 16:14:32 +0100

    simplify heredoc processes handling a LOT

--- src/eval/heredoc.c
+++ src/eval/heredoc.c
@@ -12,37 +12,16 @@
 
 #include "ush/eval.h"
 
-static inline int	output(char *data)
-{
-	ft_puts(STDOUT_FILENO, data);
-	return (EXIT_SUCCESS);
-}
-
 inline int			sh_evalheredoc(t_job *job, t_deq *toks, char **ln)
 {
 	t_tok	*tok;
-	size_t	i;
-	t_tok	*o;
 	t_proc	*p;
 
-	o = sh_tokpeek(toks);
 	tok = sh_toknext(toks);
-	i = job->procs.len - 1;
-	p = ft_vecat((t_vec *)&job->procs, i);
-	ft_isdigit(*(*ln + o->pos)) ? (p->src[0] = *(*ln + o->pos) - '0') : 0;
+	p = ft_vecback((t_vec *)&job->procs);
+	if (p->in)
+		free(p->in);
+	p->in = ft_strndup(*ln + tok->pos, tok->len);
 	sh_toknext(toks);
-	if (i > 0 && (p = job->procs.buf + i - 1)->u.fn.cb == (t_proccb *)output)
-	{
-		free(p->u.fn.data);
-		p->u.fn.data = ft_strndup(*ln + tok->pos, tok->len);
-		return (YEP);
-	}
-	if (p->kind != PROC_NONE)
-	{
-		p->piped = 1;
-		p = ft_vecput((t_vec *)&job->procs, i);
-	}
-	ps_procfn(p, (t_proccb *)output, (t_dtor)free,
-		ft_strndup(*ln + tok->pos, tok->len));
 	return (YEP);
 }
