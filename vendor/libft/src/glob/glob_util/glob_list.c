837caadea3f4e7b833ee75f5b7ad8f018536915f
JeanMax <mcanal@student.42.fr>
2018-02-23 20:45:30 +0100

    WIP on backslash

--- vendor/libft/src/glob/glob_util/glob_list.c
+++ vendor/libft/src/glob/glob_util/glob_list.c
@@ -6,24 +6,53 @@
 /*   By: mc <mc.maxcanal@gmail.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/13 09:57:56 by mc                #+#    #+#             */
-/*   Updated: 2018/02/20 17:19:43 by mcanal           ###   ########.fr       */
+/*   Updated: 2018/02/23 20:37:10 by mcanal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "glob_util.h"
 
-t_match	*matchctor(char const *path, size_t len)
+static void	sprglbcp(char *dst, char const *path)
+{
+	char		quote;
+	char		bckslsh;
+	char const	*p;
+
+	p = path;
+	quote = 0;
+	bckslsh = 0;
+	while (*p)
+	{
+		if (*p == '\'' && quote == '\'')
+			quote = 0;
+		else if (*p == '"' && quote == '"')
+			quote = 0;
+		else if (*p == '\'' && !quote)
+			quote = '\'';
+		else if (*p == '"' && !quote)
+			quote = '"';
+		else if (bckslsh)
+			;
+		else if (*p == '\\')
+			bckslsh = 1;
+		*dst = *p;
+		dst++;
+		p++;
+	}
+}
+
+t_match		*matchctor(char const *path, size_t len)
 {
 	t_match *new;
 
 	if (!(new = malloc(sizeof(t_match) + len)))
 		return ((NULL));
 	ft_bzero(new, sizeof(t_match) + len);
-	ft_memcpy(new->buf, path, len);
+	sprglbcp((char *)new->buf, path);
 	return (new);
 }
 
-void	matchdtor(t_match *match)
+void		matchdtor(t_match *match)
 {
 	t_match *prev;
 
@@ -40,13 +69,13 @@ void	matchdtor(t_match *match)
 	}
 }
 
-void	add_match_to_list(t_match *match, t_match **match_list)
+void		add_match_to_list(t_match *match, t_match **match_list)
 {
 	match->next = *match_list;
 	*match_list = match;
 }
 
-size_t	list_len(t_match *match_list)
+size_t		list_len(t_match *match_list)
 {
 	size_t	ret;
 
