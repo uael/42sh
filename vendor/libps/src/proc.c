f5dfd757f61fdb30f6656e24d36db568d6df0a30
uael <lucas.abel@yandex.com>
2018-02-24 10:52:15 +0100

    oops

--- vendor/libps/src/proc.c
+++ vendor/libps/src/proc.c
@@ -51,9 +51,9 @@ inline void			ps_procdtor(t_proc *p)
 	ft_vecdtor((t_vec *)&p->redirs, NULL);
 	p->in ? ft_pfree((void **)&p->in) : 0;
 	p->kind == PROC_FN && p->u.fn.dtor ? p->u.fn.dtor(p->u.fn.data) : 0;
-	p->u.fn.dtor = NULL;
 	p->kind == PROC_ERR && p->u.err.msg ? ft_pfree((void **)&p->u.err.msg) : 0;
 	p->kind == PROC_ERR && p->u.err.ln ? ft_pfree((void **)&p->u.err.ln) : 0;
+	p->u.fn.dtor = NULL;
 }
 
 static inline int	prepare(t_proc *prc, pid_t pgid, int *io, int fg)
