6ef0f6c9a8a74b355a5aea147791b7d052e8c7f7
JeanMax <mcanal@student.42.fr>
2018-02-25 22:11:28 +0100

    Brace: copy pattern before/after

--- vendor/libft/src/glob/glob_brace.c
+++ vendor/libft/src/glob/glob_brace.c
@@ -6,34 +6,42 @@
 /*   By: mcanal <mc.maxcanal@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 17:55:44 by mcanal            #+#    #+#             */
-/*   Updated: 2018/02/25 13:22:39 by mc               ###   ########.fr       */
+/*   Updated: 2018/02/25 21:21:32 by mc               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "glob_climb_tree.h"
 
-static int			glob_copy_pattern_and_boom(char const *pat, size_t size, \
-												t_glob_env *e)
+static int			glob_copy_pattern_and_boom(t_glob_env *e, \
+						char const *pat_end, char const *pat, size_t pat_size)
 {
 	char		brace_buf[PATH_MAX];
+	char const	*pat_save;
+	size_t		before_size;
+	size_t		after_size;
 
-	if (size + 1 > PATH_MAX)
+	before_size = glob_find_opening_brace(e->pattern, e->pattern) - e->pattern;
+	after_size = ft_strlen(pat_end);
+	if (before_size + pat_size + after_size + 1 > PATH_MAX)
 		return (GLOBUX_NOSPACE);
-	ft_memcpy(brace_buf, pat, size);
-	*(brace_buf + size) = '\0';
+	ft_memcpy(brace_buf, e->pattern, before_size);
+	ft_memcpy(brace_buf + before_size, pat, pat_size);
+	ft_memcpy(brace_buf + before_size + pat_size, pat_end, after_size + 1);
+	pat_save = e->pattern;
 	e->pattern = brace_buf;
-	return (glob_climb_tree(e));
+	glob_climb_tree(e);
+	e->pattern = pat_save;
+	return (-1);
 }
 
 static int			glob_find_sub_brace(t_glob_env *e, char const *comma, \
 										char const *pat, char const *pat_end)
 {
 	if (!comma)
-	{
-		glob_copy_pattern_and_boom(pat + 1, (size_t)(pat_end - pat) - 1, e);
-		return (-1);
-	}
-	glob_copy_pattern_and_boom(pat + 1, (size_t)(comma - pat) - 1, e);
+		return (glob_copy_pattern_and_boom(e, \
+							pat_end + 1, pat + 1, (size_t)(pat_end - pat) - 1));
+	glob_copy_pattern_and_boom(e, \
+						pat_end + 1, pat + 1, (size_t)(comma - pat) - 1);
 	return (glob_find_sub_brace(e, glob_find_comma(comma + 1, pat_end), \
 								comma, pat_end));
 }
