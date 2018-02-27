0142f4b5902d0f8b23e4d525cc8df6dce339a31a
cmalfroy <cmalfroy@student.42.fr>
2018-02-27 19:20:23 +0100

    More history options, need get_opt and finish

--- src/bi/history.c
+++ src/bi/history.c
@@ -11,15 +11,32 @@
 /* ************************************************************************** */
 
 #include "ush/bi.h"
+#include "ush/env.h"
+#include "ush/shell.h"
 
 inline int		sh_bihistory(int ac, char **av, char **env)
 {
+	char 	*home;
+	char	buf[PATH_MAX];
+
+	if(!g_sh->tty)
+		return (EXIT_FAILURE);
 	(void)env;
-	if (ac >= 2 && !ft_strcmp(av[1], "-c"))
+	if (ac == 2 && !ft_strcmp(av[1], "-c"))
 		rl_histdtor();
 	else if (ac == 3 && !ft_strcmp(av[1], "-d"))
 		rl_histdel((uint8_t)ft_atoi(av[2]));
+	else if (ac >= 2 && (!ft_strcmp(av[1], "-w") || !ft_strcmp(av[1], "-a")))
+	{
+		if ((home = sh_getenv("HOME")))
+			rl_histsave(ft_pathcat(ft_strcpy(buf, home), ".ushst"));
+	}
+	else if (ac >= 2 && (!ft_strcmp(av[1], "-r")))
+	{
+		if ((home = sh_getenv("HOME")))
+			rl_histload(ft_pathcat(ft_strcpy(buf, home), ".ushst"));
+	}
 	else if (ac == 1)
 		rl_histdump();
-	return (YEP);
+	return (EXIT_SUCCESS);
 }
