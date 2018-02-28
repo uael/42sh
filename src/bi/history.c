f436885517f576be33f6529acc1a15c667fb24f4
cmalfroy <cmalfroy@student.42.fr>
2018-02-28 11:23:35 +0100

    get_opt set in history, need tests and finish options

--- src/bi/history.c
+++ src/bi/history.c
@@ -14,27 +14,81 @@
 #include "ush/env.h"
 #include "ush/shell.h"
 
-inline int		sh_bihistory(int ac, char **av, char **env)
+enum 				u_opt
+{
+	HISTORY_C = 1 << 1,
+	HISTORY_D = 1 << 2,
+	HISTORY_W = 1 << 3,
+	HISTORY_A = 1 << 4,
+	HISTORY_R =  1 << 5,
+	HISTORY_P = 1 << 6,
+	HISTORY_S = 1 << 7
+};
+
+typedef	struct		s_opt
+{
+	uint8_t flags;
+	uint8_t offset;
+	char 	*filename;
+	char 	*arg;
+}					t_opt;
+
+static inline void	optinit(t_opt *opts)
+{
+	opts->flags = 0;
+	opts->offset = 0;
+	opts->filename = ".ushst";
+	opts->arg = NULL;
+}
+
+static inline int	histparse(int ac, char *av[], t_opt *opts)
+{
+	int		opt;
+
+	g_optind = 1;
+	optinit(opts);
+	while ((opt = ft_getopt(ac, av, "cr:a:w:d:p:s:")) != -1)
+		if (opt == 'r' && (opts->flags |= HISTORY_R))
+			opts->filename = g_optarg;
+		else if (opt == 'a' && (opts->flags |= HISTORY_A))
+			opts->filename = g_optarg;
+		else if (opt == 'w' && (opts->flags |= HISTORY_W))
+			opts->filename = g_optarg;
+		else if (opt == 'd' && (opts->flags |= HISTORY_D))
+			opts->offset = (uint8_t)ft_atoi(g_optarg);
+		else if (opt == 'c')
+			opts->flags |= HISTORY_C;
+		else if (opt == 'p' && (opts->flags |= HISTORY_P))
+			opts->arg = g_optarg;
+		else if (opt == 's' && (opts->flags |= HISTORY_S))
+			opts->arg = g_optarg;
+		else
+			return (WUT);
+	return (g_optind);
+}
+
+inline int			sh_bihistory(int ac, char **av, char **env)
 {
 	char 	*home;
 	char	buf[PATH_MAX];
+	t_opt	opt;
 
-	if(!g_sh->tty)
-		return (EXIT_FAILURE);
 	(void)env;
-	if (ac == 2 && !ft_strcmp(av[1], "-c"))
+	if (!g_sh->tty || histparse(ac, av, &opt) < 0)
+		return (EXIT_FAILURE);
+	if (opt.flags & HISTORY_C)
 		rl_histdtor();
-	else if (ac == 3 && !ft_strcmp(av[1], "-d"))
-		rl_histdel((uint8_t)ft_atoi(av[2]));
-	else if (ac >= 2 && (!ft_strcmp(av[1], "-w") || !ft_strcmp(av[1], "-a")))
+	else if (opt.offset && (opt.flags & HISTORY_D))
+		rl_histdel(opt.offset);
+	else if (opt.filename && ((opt.flags & HISTORY_W) || opt.flags & HISTORY_A))
 	{
 		if ((home = sh_getenv("HOME")))
-			rl_histsave(ft_pathcat(ft_strcpy(buf, home), ".ushst"));
+			rl_histsave(ft_pathcat(ft_strcpy(buf, home), opt.filename));
 	}
-	else if (ac >= 2 && (!ft_strcmp(av[1], "-r")))
+	else if (opt.filename && (opt.flags & HISTORY_R))
 	{
 		if ((home = sh_getenv("HOME")))
-			rl_histload(ft_pathcat(ft_strcpy(buf, home), ".ushst"));
+			rl_histload(ft_pathcat(ft_strcpy(buf, home), opt.filename));
 	}
 	else if (ac == 1)
 		rl_histdump();
