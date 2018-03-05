8279fb3e3d3b9985f171c92c253e7b2267bdc673
JeanMax <mcanal@student.42.fr>
2018-03-05 22:33:27 +0100

    More tests

--- test/glob.sh
+++ test/glob.sh
@@ -13,17 +13,17 @@ echo [24][21]"sh"
 echo [24][21]'sh'
 echo "[24][21]sh"
 echo '[24][21]sh'
+echo *
 echo s?c ob? ???? ?
 echo [!s]bj [!owf1-9]rc
-# echo [A-Z]*
-# echo *
+echo src/[a-zA-Z]*.c
 echo {src,obj}
 echo {a,c,b}
 echo {a}
+echo */*/*.{h,c,o}
 echo {src,obj}/*
 echo {src,obj}/*/{h,c,o}
-# echo {src,obj}/*/*.{h,c,o}
-# echo {src,obj,}/*/*.{h,c,o}
-# echo {src,obj,include}/*/*.{h,c,o}
-echo */*/*.{h,c,o}
-# echo {{src,obj},include}/*/*.{h,c,o}
+echo {src,obj}/*/*.{h,c,o}
+echo {src,obj,}/*/*.{h,c,o}
+echo {src,obj,include}/*/*.{h,c,o}
+echo {{src,obj},include}/*/*.{h,c,o}
