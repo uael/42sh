8bec4ba03fbc396438bf1edaa05379859feff7f6
JeanMax <mcanal@student.42.fr>
2018-03-05 09:30:41 +0100

    Add some glob tests

--- test/glob.sh
+++ test/glob.sh
@@ -13,3 +13,17 @@ echo [24][21]"sh"
 echo [24][21]'sh'
 echo "[24][21]sh"
 echo '[24][21]sh'
+echo s?c ob? ???? ?
+echo [!s]bj [!owf1-9]rc
+# echo [A-Z]*
+# echo *
+echo {src,obj}
+echo {a,c,b}
+echo {a}
+echo {src,obj}/*
+echo {src,obj}/*/{h,c,o}
+# echo {src,obj}/*/*.{h,c,o}
+# echo {src,obj,}/*/*.{h,c,o}
+# echo {src,obj,include}/*/*.{h,c,o}
+echo */*/*.{h,c,o}
+# echo {{src,obj},include}/*/*.{h,c,o}
